378534fcc0d4e4801c80c4a7ffc3e1965ab66a9a
root\emrah <kirmiziemrahcsharp@gmail.com>
2022-08-15 20:55:24 +0300

    EasyMath1.1

--- EasyMath/app/src/main/java/com/emrahkirmizi/easymath/MainActivity.kt
+++ EasyMath/app/src/main/java/com/emrahkirmizi/easymath/MainActivity.kt
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var myButton1: Button
     private lateinit var myButton2: Button
     private lateinit var myButton3: Button
+    private lateinit var myButton4: Button
+    private lateinit var myButton5: Button
     private lateinit var myTextView1: TextView
     private lateinit var myTextView2: TextView
     private lateinit var myTextViewSonuc: TextView
@@ -25,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         myButton1 = findViewById(R.id.button1)
         myButton2 = findViewById(R.id.button2)
         myButton3 = findViewById(R.id.button3)
+        myButton4 = findViewById(R.id.button4)
+        myButton5 = findViewById(R.id.button5)
         myTextView1 = findViewById(R.id.textView1)
         myTextView2 = findViewById(R.id.textView2)
         myTextViewSonuc = findViewById(R.id.textViewSonuc)
@@ -35,44 +37,98 @@ class MainActivity : AppCompatActivity() {
     fun buttonPressed(view: View) {
         val myRandomInt1 = Random.nextInt(1, 10)
         val myRandomInt2 = Random.nextInt(1, 10)
-        val myRandomYanlisSonuc = Random.nextInt(1, 100)
+        val myRandomYanlisSonuc1 = Random.nextInt(1, 100)
+        val myRandomYanlisSonuc2 = Random.nextInt(1, 100)
+        val myRandomYanlisSonuc3 = Random.nextInt(1, 100)
+
         val dogruSonuc = myRandomInt1 * myRandomInt2
 
 
-        if (myRandomInt1 >= myRandomInt2) {
+        if (myRandomInt1 > myRandomInt2) {
             myTextView1.text = "$myRandomInt1 * $myRandomInt2"
             myButton2.text = "$dogruSonuc"
-            myButton3.text = "$myRandomYanlisSonuc"
+            myButton3.text = "$myRandomYanlisSonuc1"
+            myButton4.text = "$myRandomYanlisSonuc2"
+            myButton5.text = "$myRandomYanlisSonuc3"
 
-            myButton2.setOnClickListener{
+
+            myButton2.setOnClickListener {
                 myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
             }
             myButton3.setOnClickListener {
+                myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
+            }
+            myButton4.setOnClickListener {
+                myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
+            }
+            myButton5.setOnClickListener {
+                myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
+            }
+
+        } else if (myRandomInt1 <= myRandomInt2) {
+            myTextView1.text = "$myRandomInt1 * $myRandomInt2"
+            myButton2.text = "$myRandomYanlisSonuc1"
+            myButton3.text = "$dogruSonuc"
+            myButton4.text = "$myRandomYanlisSonuc2"
+            myButton5.text = "$myRandomYanlisSonuc3"
+
+            myButton2.setOnClickListener {
+                myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
+            }
+            myButton3.setOnClickListener {
+                myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
+            }
+            myButton4.setOnClickListener {
+                myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
+            }
+            myButton5.setOnClickListener {
+                myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
+            }
+
+        } else if (myRandomInt1 >= myRandomInt2) {
+            myTextView1.text = "$myRandomInt1 * $myRandomInt2"
+            myButton2.text = "$myRandomYanlisSonuc1"
+            myButton3.text = "$myRandomYanlisSonuc2"
+            myButton4.text = "$dogruSonuc"
+            myButton5.text = "$myRandomYanlisSonuc3"
+
+            myButton2.setOnClickListener {
+                myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
+            }
+            myButton3.setOnClickListener {
+                myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
+            }
+            myButton4.setOnClickListener {
+                myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
+            }
+            myButton5.setOnClickListener {
                 myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
             }
 
         } else {
             myTextView1.text = "$myRandomInt1 * $myRandomInt2"
-            myButton3.text = "$dogruSonuc"
-            myButton2.text = "$myRandomYanlisSonuc"
+            myButton2.text = "$myRandomYanlisSonuc1"
+            myButton3.text = "$myRandomYanlisSonuc2"
+            myButton4.text = "$myRandomYanlisSonuc3"
+            myButton5.text = "$dogruSonuc"
 
-            myButton2.setOnClickListener{
+            myButton2.setOnClickListener {
                 myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
             }
             myButton3.setOnClickListener {
+                myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
+            }
+            myButton4.setOnClickListener {
+                myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
+            }
+            myButton5.setOnClickListener {
                 myTextViewSonuc.text = "dogru sonuc = $dogruSonuc"
             }
 
         }
 
 
-
-
-
-
     }
-
-
 
 
 }
